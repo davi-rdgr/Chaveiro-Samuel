21754e4cac277e1247511c122dc9188cd8ecd8ce
davi-rdgr <davirdgrsouza@gmail.com>
2025-06-28 14:23:16 -0300

    Primeiro commit do inicio do desenvolvimento da página do Chaveiro Samuel.

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+let logocontent = document.querySelector('.logos');
+
+for (let i = 0; i < 10; i++) {
+    let foto1 = document.createElement("img");
+    foto1.src = "./assets/images/imagem_cliente.png"
+    foto1.alt = "oba oba"
+    logocontent.appendChild(foto1);
+}
+let logocontent2 = document.querySelector('.logos2');
+
+for (let i = 0; i < 10; i++) {
+    let foto1 = document.createElement("img");
+    foto1.src = "./assets/images/imagem_cliente.png"
+    foto1.alt = "oba oba"
+    logocontent2.appendChild(foto1);
+}
